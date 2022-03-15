797919d0e5fa083430f41efe224c74c2c1dceca3
amankori2307 <amankori2307@gmail.com>
2022-03-15 19:07:34 +0530

    Added TODO module

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoRoutingModule } from './todo-routing.module';
+import { TodoComponent } from './todo.component';
+
+
+@NgModule({
+  declarations: [
+    TodoComponent
+  ],
+  imports: [
+    CommonModule,
+    TodoRoutingModule
+  ],
+  exports: [
+    TodoComponent
+  ]
+})
+export class TodoModule { }
